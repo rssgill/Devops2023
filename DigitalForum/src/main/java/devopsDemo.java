9dcbf8d7dc0cf08d35b3afde8785030dad91c773
rssgill <50801762+rssgill@users.noreply.github.com>
2023-04-07 14:10:49 -0400

    second code

--- DigitalForum/src/main/java/devopsDemo.java
+++ DigitalForum/src/main/java/devopsDemo.java
@@ -4,6 +4,8 @@ public class devopsDemo {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello, This is Devops first code.");
+		
+		System.out.println("Hello, This is Devops second code.");
 
 	}
 
